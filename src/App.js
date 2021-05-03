c26f50e7c5a44e13fee82dd3c84300c7ff611898
Abdulrahman-19 <abedabogneem@gmail.com>
2021-05-04 01:40:11 +0300

    ubdate

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
+import Footer from './components/footer';
+import Header from './components/header';
+import Main from './components/main';
 
 export class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      updateSearch : '',
-      data : '',
-      imgSource : ''
-    };
-  }
-  getLocation = async (e)=>{
-    e.preventDefault();
-    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=${this.state.updateSearch}&format=json` ;
-    const req = await axios.get(url);
-    let lat = req.data[0].lat;
-    let lon = req.data[0].lon;
-    console.log(req.data);
-    this.setState({
-      data : req.data[0],
-      imgSource: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_KEY}&q=&center=${lat},${lon}&zoom=10`
-    });
-  };
-  updateSearchQuery = (e) => {
-    this.setState({
-      updateSearch : e.target.value
-    });
-    console.log(this.state.updateSearch);
-  }
+
   render() {
     return (
       <div>
-        <h1>City Explorer</h1>
-        <form onSubmit= {this.getLocation}>
-          <input type = 'text' onChange = {this.updateSearchQuery} placeholder = 'City Name'></input>
-          <input type = 'submit' value = 'get city'/>
-        </form>
-        <p>{this.state.data.display_name}</p>
-        <img src={this.state.imgSource} alt = ''/>
+        <Header/>
+        <Main/>
+        <Footer/>
       </div>
     );
   }
